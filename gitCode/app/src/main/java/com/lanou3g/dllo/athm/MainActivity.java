3c9b774759f9ad46901315df38d2b8b2f3649fbf
hes0918 <517878710@qq.com>
2016-09-08 19:03:56 +0800

    创建了一个汽车之家APP

---
+++ gitCode/app/src/main/java/com/lanou3g/dllo/athm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lanou3g.dllo.athm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
