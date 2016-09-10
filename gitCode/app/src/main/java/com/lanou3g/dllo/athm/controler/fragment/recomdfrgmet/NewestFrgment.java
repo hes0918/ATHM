dea91da5ae806fa50db4505ae9ba0dad30d89ff1
hes0918 <517878710@qq.com>
2016-09-10 08:32:42 +0800

    搭好推荐页面的TabLayout

---
+++ gitCode/app/src/main/java/com/lanou3g/dllo/athm/controler/fragment/recomdfrgmet/NewestFrgment.java
@@ -0,0 +1,24 @@
+package com.lanou3g.dllo.athm.controler.fragment.recomdfrgmet;
+
+import com.lanou3g.dllo.athm.R;
+import com.lanou3g.dllo.athm.controler.fragment.AbsBaseFragment;
+
+/**
+ * Created by dllo on 16/9/9.
+ */
+public class NewestFrgment extends AbsBaseFragment {
+    @Override
+    protected int setLayout() {
+        return R.layout.newest_fragment;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
